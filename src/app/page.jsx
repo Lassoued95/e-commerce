7c0273195f9c270d27577872c1b3cf2ce2d56d76
Mohamed Lassoued <lassouedmohamed2004@gmail.com>
2024-10-17 19:23:20 +0100

    modification en image de font

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,8 +7,23 @@ export default function Home() {
     <div className="min-h-screen bg-gray-100">
       {/* About Section */}
       <About />
-      <div className="w-[90%] h-[400px] bg-slate-200  ml-14 mt-2  rounded-tr-[40px] rounded-bl-[40px]">
-        
+      <div className="w-[80%] h-[450px] bg-slate-200  ml-[8%] mt-2  rounded-tr-[40px] rounded-bl-[40px]">
+        <img 
+          src="air.png"
+          className="ml-[55%] h-[450px] "
+        />
+      <div class="bg-black text-center py-20 mt-[-37%] h-[450px] animate-zoomIn">
+  <h1 class="text-4xl md:text-6xl font-bold text-white tracking-wide uppercase mb-4">
+    Step Into Comfort and Style
+  </h1>
+  <p class="text-lg md:text-2xl text-gray-200 mb-6">
+    Discover the latest collections designed for every adventure.
+  </p>
+  <button class="mt-4 bg-white text-blue-800 font-bold py-3 px-6 rounded-lg shadow-md hover:bg-gray-100 hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+    Shop Now
+  </button>
+</div>
+
       </div>
 
       {/* Image Section */}
